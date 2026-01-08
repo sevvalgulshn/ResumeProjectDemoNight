d3dd908e52719340fd8fea818d8ef8493e312283
MuratYucedag <yucedagmurat23@gmail.com>
2026-01-08 22:11:35 +0300

    ...

---
+++ ResumeProjectDemoNight/ViewComponents/DefaultViewComponents/_DefaultHomeComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ResumeProjectDemoNight.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultHomeComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
