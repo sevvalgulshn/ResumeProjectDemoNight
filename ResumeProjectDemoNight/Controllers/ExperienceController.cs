92ccda35c285de366bff0dc95f81b4b7a4c02841
MuratYucedag <yucedagmurat23@gmail.com>
2026-01-20 22:12:09 +0300

    ...

---
+++ ResumeProjectDemoNight/Controllers/ExperienceController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResumeProjectDemoNight.Context;
+using ResumeProjectDemoNight.Entities;
+
+namespace ResumeProjectDemoNight.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        private readonly ResumeContext _context;
+        public ExperienceController(ResumeContext context)
+        {
+            _context = context;
+        }
+        public IActionResult ExperienceList()
+        {
+            var values = _context.Experiences.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateExperience()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateExperience(Experience experience)
+        {
+            _context.Experiences.Add(experience);
+            _context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        public IActionResult DeleteExperience(int id)
+        {
+            var value = _context.Experiences.Find(id);
+            _context.Experiences.Remove(value);
+            _context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateExperience(int id)
+        {
+            var value = _context.Experiences.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateExperience(Experience experience)
+        {
+            _context.Experiences.Update(experience);
+            _context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+    }
+}
